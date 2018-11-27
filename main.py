2b908089206496c6ce119e8e284b731b5cb7e8a2
yohei <youhei@youhei.local>
2018-11-27 13:12:11 +0900

    mysql test

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 from settings import cmn
 import requests
-from bs4 import BeautifulSoup
+
 
 # セッションを開始
 session = requests.session()
-
-# ログイン
-login_info = {
-    "login_field":'yohei29',
-    "password":'yohan484803',
-}
-
-# action
-url_login = "https://github.com/session"
-res = session.post(url_login, data=login_info)
-res.raise_for_status() # エラーならここで例外を発生させる
-
-print(res.text)
---
+++ mysql.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("..")
+
+from settings import sql
+import MySQLdb
+
+con = MySQLdb.connect(
+    db=sql.DB_NAME,
+    user=sql.DB_USER,
+    passwd=sql.DB_PASS,
+    charset=sql.DB_CHAR
+)
+print(con)
+c = con.cursor()
+c.execute('drop tables if exists cities')
+c.execute('''
+create table cities(
+cities_rank integer,
+city text,
+population integer
+)
+''')
+
+con.commit()
+con.close()

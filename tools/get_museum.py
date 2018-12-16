16c119f7bf25c816de1753b719cde2234dd430ea
yohei <yohan.k.95@gmail.com>
2018-12-16 11:08:29 +0900

    緯度経度クエリ追加

--- tools/get_museum.py
+++ tools/get_museum.py
@@ -8,6 +8,23 @@ sparql.setQuery(r'''
     FILTER REGEX(?address, "^\\p{Han}{2,3}[都道府県]")
   } ORDER BY ?s
   ''')
+# 緯度経度の情報も取得
+# sparql.setQuery(r'''
+#  SELECT * WHERE {
+#   ?s rdf:type dbpedia-owl:Museum;
+#   prop-ja:所在地 ?address .
+#   OPTIONAL {?s rdfs:label ?label .}
+#   OPTIONAL {
+#       ?s prop-ja:経度度 ?lon_degree;
+#       prop-ja:経度分 ?lon_minute;
+#       prop-ja:経度秒 ?lon_second;
+#       prop-ja:緯度度 ?lat_degree;
+#       prop-ja:緯度分 ?lat_minute;
+#       prop-ja:緯度秒 ?lat_second .
+#   }
+#   FILTER REGEX(?address, "^\\p{Han}{2, 3}[都道府県]")
+# } ORDER BY ?s
+# ''')
 
 sparql.setReturnFormat('json')
 res = sparql.query().convert()
